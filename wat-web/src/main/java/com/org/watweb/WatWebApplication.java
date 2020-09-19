f95d710d822e278904d6e60eee2342374265e97c
giamp <giampaoloconca168@gmail.com>
2020-09-19 11:58:06 +0200

    Added Module WEB and test bootstrap for springboot

---
+++ wat-web/src/main/java/com/org/watweb/WatWebApplication.java
@@ -0,0 +1,13 @@
+package com.org.watweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatWebApplication.class, args);
+	}
+
+}
---
+++ wat-web/src/main/java/com/org/watweb/controllers/LoginController.java
@@ -0,0 +1,13 @@
+package com.org.watweb.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping({"", "/"})
+    public String toLogin() {
+        return "login";
+    }
+}
