d9244285daa33d189d322808f0dc35716b1c88be
giamp <giampaoloconca168@gmail.com>
2020-09-19 11:18:13 +0200

    First Commit

---
+++ wat-data/src/main/java/com/org/watdata/WatDataApplication.java
@@ -0,0 +1,13 @@
+package com.org.watdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatDataApplication.class, args);
+	}
+
+}
---
+++ wat-data/src/test/java/com/org/watdata/WatDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.watdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
